d3bffba68c303538b1d167ce41bdff223d5b88a9
sonardude-sr <76888556+sonardude-sr@users.noreply.github.com>
2021-01-07 17:16:39 -0800

    Add files via upload

--- graph.py
+++ graph.py
@@ -2,3 +2,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+def get_list(a, b, c):
+    values = [n for n in range(a, b + c, c)]
+    return values
+
+
+x_window = get_list(0, 10, 1)
+y_window = get_list(0, 10, 1)
+X, Y = np.meshgrid(x_window, y_window)
+plt.quiver(X, Y, X, Y)
+plt.show()
