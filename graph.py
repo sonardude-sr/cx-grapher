33d23f11a576eafa721634a276c9edf008b4f504
sonardude-sr <76888556+sonardude-sr@users.noreply.github.com>
2021-01-09 15:49:07 -0800

    Add files via upload

--- graph.py
+++ graph.py
@@ -9,26 +9,36 @@ def get_list(a, b, c):
 
 
 # def transform_array(a, add, multiply):
-#     out_list = []
+#     complex_list = []
 #     for i in a:
 #         accumulator = []
 #         for j in i:
 #             accumulator.append((multiply * j) + add)
-#         out_list.append(accumulator)
-#     return out_list
+#         complex_list.append(accumulator)
+#     return complex_list
 
 
 def transform_meshgrid(x_mesh, y_mesh):
-    list_out = []
+    accumulator_list = []
+    imaginary_parts = []
+    real_parts = []
     for i, j in zip(np.array(x_mesh).flatten(), np.array(y_mesh).flatten()):
         n = complex(i, j)
-        list_out.append(n)
-    return list_out
+        n = n ** 2
+        accumulator_list.append(n)
+    for i in accumulator_list:
+        real_parts.append(i.real)
+        imaginary_parts.append(i.imag)
+    return real_parts, imaginary_parts
 
-x_window = get_list(0, 50, 5)
-y_window = get_list(0, 50, 5)
+
+x_window = get_list(-5, 5, 1)
+y_window = get_list(-5, 5, 1)
 X, Y = np.meshgrid(x_window, y_window)
-transform_meshgrid(X, Y)
-# plt.quiver(X, Y, transform_array(X, 0, 1), transform_array(Y, 0, 1))
-# plt.show()
+U, V = transform_meshgrid(X, Y)
+U = np.array(U)
+V = np.array(V)
+plt.quiver(X, Y, U - X.flatten(), V - Y.flatten())
+
+plt.show()
 
