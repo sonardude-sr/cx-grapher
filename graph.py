10c41a15e99261205c2636488e7f3b47796642ee
sonardude-sr <76888556+sonardude-sr@users.noreply.github.com>
2021-01-09 20:44:26 -0800

    Add files via upload

--- graph.py
+++ graph.py
@@ -14,7 +14,7 @@ def transform_mesh_grid(x_mesh, y_mesh):
     real_parts = []
     for i, j in zip(np.array(x_mesh).flatten(), np.array(y_mesh).flatten()):
         n = complex(i, j)
-        n = n ** 2
+        n = n + 1
         accumulator_list.append(n)
     for i in accumulator_list:
         real_parts.append(i.real)
@@ -22,13 +22,17 @@ def transform_mesh_grid(x_mesh, y_mesh):
     return real_parts, imaginary_parts
 
 
-x_window = get_list(-100, 100, 20)
-y_window = get_list(-100, 100, 20)
-X, Y = np.meshgrid(x_window, y_window)
-U, V = transform_mesh_grid(X, Y)
-U = np.array(U)
-V = np.array(V)
-plt.quiver(X, Y, U - X.flatten(), V - Y.flatten())
-plt.xlabel("Real Numbers")
-plt.ylabel("Imaginary Numbers")
-plt.show()
+def create_plot():
+    x_window = get_list(-5, 5, 1)
+    y_window = get_list(-5, 5, 1)
+    x, y = np.meshgrid(x_window, y_window)
+    u, v = transform_mesh_grid(x, y)
+    u = np.array(u)
+    v = np.array(v)
+    plt.quiver(x, y, u - x.flatten(), v - y.flatten())
+    plt.xlabel("Real Numbers")
+    plt.ylabel("Imaginary Numbers")
+    plt.show()
+
+
+create_plot()
